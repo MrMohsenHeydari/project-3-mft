8b2849c9f121d8373e26a1aff610541e548be191
MrMohsenHeydari <mohsen.heydari061@gmail.com>
2024-09-29 14:44:39 -0700

    ADD JS & JQUERY

---
+++ js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded",()=>{
+
+    $('.first').backstretch([
+        "../assets/Image/slideshow/1.jpg", 
+        "../assets/Image/slideshow/2.jpg",
+        "../assets/Image/slideshow/3.jpg"
+      ],  {duration: 2000, fade: 850});
+    
+    
+})
+
+
+
