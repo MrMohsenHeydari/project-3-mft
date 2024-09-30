6b2714e6ef0a5375d3e2496d72bfaef5a04c26ca
MrMohsenHeydari <mohsen.heydari061@gmail.com>
2024-09-30 14:18:01 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ window.addEventListener("DOMContentLoaded",()=>{
         "../assets/Image/slideshow/2.jpg",
         "../assets/Image/slideshow/3.jpg"
       ],  {duration: 2000, fade: 850});
-    
-    
+
 })
 
 
